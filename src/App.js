83b1dc2075142c7e0bb116c24ac7fac88e21fa27
zackvuldin <y.zachary@gmail.com>
2020-11-22 21:12:22 -0800

    update to front

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
 					href='https://github.com/zackvuldin/Amethyst-Arcade/tree/main/div_collision'
 					target='_blank'
 					rel='noopener noreferrer'>
-					div collision
+					Div Collision
 				</a>
 				<a
 					className='App-link'
@@ -58,7 +58,7 @@ function App() {
 					href='https://reactjs.org'
 					target='_blank'
 					rel='noopener noreferrer'>
-					asteroids
+					Asteroids
 				</a>
 				<a
 					className='App-link'
@@ -67,6 +67,75 @@ function App() {
 					rel='noopener noreferrer'>
 					Hanoi Tower
 				</a>
+				<p>Reactions from happy gamers!</p>
+				<p>Scroll Down</p>
+			</header>
+			<header className='Anime-header'>
+				<img
+					src='https://media.giphy.com/media/cl9qyXsTrOSqs/giphy.gif'
+					className='Anime'
+					alt='logo'
+				/>
+				<img
+					src='https://media.tenor.com/images/1d7c71aeb9645810b0fdaed0abd3d764/tenor.gif'
+					className='Anime'
+					alt='logo'
+				/>
+				<img
+					src='https://i.pinimg.com/originals/d2/68/de/d268de2f894c1b4788fe845adf3a2d79.gif'
+					className='Anime'
+					alt='logo'
+				/>
+				<img
+					src='https://media1.tenor.com/images/2224ecfbcdaec87c7fb96dae251c026d/tenor.gif?itemid=12345692'
+					className='Anime'
+					alt='logo'
+				/>
+				<img
+					src='https://i.pinimg.com/originals/b2/ea/e1/b2eae1eeef0e700053af25e42bcc2013.gif'
+					className='Anime'
+					alt='logo'
+				/>
+				<img
+					src='https://data.whicdn.com/images/121399568/original.gif'
+					className='Anime'
+					alt='logo'
+				/>
+				<img
+					src='https://media1.tenor.com/images/70a951cddcff528e9b71221adcff4d9f/tenor.gif?itemid=5296012'
+					className='Anime'
+					alt='logo'
+				/>
+				<img
+					src='https://image.myanimelist.net/ui/7TVWLJ4cRvwHjFyWCI7sZwBX6u_h5EYal5_hKBSEC-r3u9jipY99cGBWP0RzDzn3xgFFRbjEDr0Ftn0n1wjiPEjO3yprhxbIcq5Pi4xvjWOD4OdmsG__wvl9o_kW5QHa'
+					className='Anime'
+					alt='logo'
+				/>
+				<img
+					src='https://media2.giphy.com/media/3o84TYPDosWEMOJvby/giphy.gif'
+					className='Anime'
+					alt='logo'
+				/>
+				<img
+					src='https://68.media.tumblr.com/4390e789d5d054e16f1684de53f504d2/tumblr_mwa6othOwn1r60b55o1_500.gif'
+					className='Anime'
+					alt='logo'
+				/>
+				<img
+					src='https://thumbs.gfycat.com/SimplisticAjarHorse-size_restricted.gif'
+					className='Anime'
+					alt='logo'
+				/>
+				<img
+					src='https://cdn.lowgif.com/full/880517c583d6fd4d-stella-vermillion-gif-tumblr.gif'
+					className='Anime'
+					alt='logo'
+				/>
+				<img
+					src='https://image.myanimelist.net/ui/_3fYL8i6Q-n-155t3dn_4jDknYN0aCPtudyMf63Csj0WcqbLRuyEIHKcG7ADvf27'
+					className='Anime'
+					alt='logo'
+				/>
 			</header>
 		</div>
 	);
