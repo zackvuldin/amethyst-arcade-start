be6551ffc0c7a7d0dc2f3e8d54d35e2320ca0358
zackvuldin <y.zachary@gmail.com>
2020-11-22 18:30:08 -0800

    frontpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import home from './Home/home'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className='App'>
+			<header className='App-header'>
+				<p>Welcome to Amethyst Arcade</p>
+				<img
+					src='https://lh3.googleusercontent.com/proxy/J40T7pR8RDzMuSDLUWbGywaRaCY9_58D6vhu0y-cRyhgsan4IL-JFPE8HhgV1I_2i_RcNa568Mzc6i5dldGSm7BicayL3z6PNgE5eN8baFu-H_rqLa86xB2NXXV47RbBy012e5-lm1g'
+					className='App-logo'
+					alt='logo'
+				/>
+				<a
+					className='App-link'
+					href='https://github.com/zackvuldin/Amethyst-Arcade/tree/main/snek'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Snake
+				</a>
+				<a
+					className='App-link'
+					href='https://github.com/zackvuldin/Amethyst-Arcade/tree/main/aim_trainer'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Aimer
+				</a>
+				<a
+					className='App-link'
+					href='https://github.com/zackvuldin/Amethyst-Arcade/tree/main/div_collision'
+					target='_blank'
+					rel='noopener noreferrer'>
+					div collision
+				</a>
+				<a
+					className='App-link'
+					href='https://github.com/zackvuldin/Amethyst-Arcade/tree/main/chess'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Chess
+				</a>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Pong
+				</a>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Platformer
+				</a>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'>
+					asteroids
+				</a>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Hanoi Tower
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Home/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { Link } from 'react-router';
+
+class List extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>Please choose a repository from the list below.</p>
+				<ul>
+					<li>
+						{/* <Link to='/react'>React</Link> */}
+					</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default List;
